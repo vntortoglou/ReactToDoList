bf764e3fa583596be8269ef9a4fcc9210434b262
Vangelis <vntortoglou@gmail.com>
2025-04-04 17:51:45 +0300

    Added a Close Button

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,30 @@ export default function ToDoList() {
     setInputValue("");
   }
 
+  function handleRemove(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
+
   const listItems = items.map((item) => (
-    <li key={item.id}>{item.description}</li>
+    <li key={item.id} className="flex items-center justify-between mb-2">
+      {item.description}
+      <button
+        onClick={() => handleRemove(item.id)}
+        className="ml-4 text-white bg-red-500"
+      >
+        ✕
+      </button>
+    </li>
   ));
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="toDo">toDo:</label>
+      <form onSubmit={handleSubmit} className="mb-4">
+        <label htmlFor="toDo" className="block text-white mb-2">
+          toDo:
+        </label>
         <input
+          className="randomInput p-2 mr-2 rounded"
           id="toDo"
           type="text"
           value={inputValue}
@@ -37,13 +52,13 @@ export default function ToDoList() {
             setInputValue(event.target.value);
           }}
         />
-<button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
-  Sketch
-</button>
+        <button className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+          Add
+        </button>
       </form>
       <section>
         <ul>{listItems}</ul>
       </section>
     </>
-  )
+  );
 }
